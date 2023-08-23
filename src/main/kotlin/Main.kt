07ef499c65271cbfa9668357499dd851808df57f
Pavel Kornienkov <122346047+CapriKorP@users.noreply.github.com>
2023-08-23 17:39:53 +0300

    Add immutable variable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,28 @@
 fun main() {
     val payMethod = "Visa"
     var lastPayment = 75000
-    val transaction = 5000
+    val transaction = 10000
 
     println("Комиссия составила ${calculate(payMethod, lastPayment, transaction)} рублей")
 }
 
-fun calculate(payMethod: String, lastPayment: Int, transaction: Int): Int {
+fun calculate(payMethod: String = "VK Pay", lastPayment: Int = 0, transaction: Int): Int {
+    val maxMonthTransaction = 75000
     val minCommission = 35
     val commissionPercent = 0.75
+    val dailyLimitVkPay = 15000
+    val monthLimitVkPay = 40000
+    val dailyLimitCard = 150000
+    val monthLimitCard = 600000
 
     val commission = when (payMethod) {
         "Mastercard", "Maestro" -> {
-            if (lastPayment > 75000) {
+            if (lastPayment + transaction > maxMonthTransaction) {
                 (transaction * 0.6 / 100) + 20
             } else {
                 0
             }
         }
-
         "Visa", "МИР" -> if (transaction * commissionPercent / 100 <= minCommission) minCommission.toDouble() else transaction * commissionPercent / 100
         "VK Pay" -> 0
         else -> 0
